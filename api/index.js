e75d72e006539dcd6083f58e815fa8b89b811196
Syed Asifullah <syedasifullah00@gmail.com>
2024-07-03 17:54:12 +0500

    create a userschema using mongoose

--- api/index.js
+++ api/index.js
@@ -1,7 +1,9 @@
 import express from "express";
-
+import mongoose, { mongo } from "mongoose";
 const app = express();
-
+mongoose.connect(
+  "mongodb+srv://khanasfii00:asifmern@cluster0.xbuzzyc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+);
 app.listen(3000, () => {
   console.log("server is running on 3000.");
 });
