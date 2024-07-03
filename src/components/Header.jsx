23ee9120e971f299d6c19331f5a4ff35d35df4cf
Syed Asifullah <syedasifullah00@gmail.com>
2024-07-03 11:47:58 +0500

    comp and pages created along with react-router-dom

---
+++ src/components/Header.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { FaSearch } from "react-icons/fa";
+const Header = () => {
+  return (
+    <header className="bg-slate-200 shadow-md">
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+        <Link to="/">
+          <h1 className="font-bold text-sm sm:text-xl flex">
+            <span className="text-slate-500">Mern</span>
+            <span className="text-slate-700">Estate</span>
+          </h1>
+        </Link>
+        <form className="bg-slate-100 p-3 rounded-lg flex items-center">
+          <input
+            className="bg-transparent focus:outline-none w-24 sm:w-64"
+            type="text"
+            placeholder="Search..."
+          />
+          <FaSearch className="text-slate-600" />
+        </form>
+        <ul className="flex gap-4">
+          <Link to="/home">
+            <li className="hidden sm:inline hover:underline text-slate-700">
+              Home
+            </li>
+          </Link>
+          <Link to="/about">
+            <li className="hidden sm:inline hover:underline text-slate-700">
+              About
+            </li>
+          </Link>
+          <Link to="/sing-in">
+            <li className=" hover:underline text-slate-700">Sign in</li>
+          </Link>
+        </ul>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
