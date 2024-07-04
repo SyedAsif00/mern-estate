e1839ff046fd4f71949251778f3b966db117f71e
Syed Asifullah <syedasifullah00@gmail.com>
2024-07-04 13:43:48 +0500

    middle ware and error handler functions

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import User from "../modals/users.modal.js";
 import bcryptjs from "bcrypt";
+import { errorHandler } from "../utils/error.js";
 export const signUp = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashedPass = bcryptjs.hashSync(password, 10);
@@ -10,6 +11,6 @@ export const signUp = async (req, res, next) => {
     res.status(201).json("user created successfully!!");
   } catch (error) {
     // res.status(500).json(error.message);
-    next(error);
+    next(errorHandler(550, "server issues"));
   }
 };
