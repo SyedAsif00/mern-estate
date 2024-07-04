541321c14d3806e72a83e8028bc98f5145b356a8
Syed Asifullah <syedasifullah00@gmail.com>
2024-07-04 15:26:34 +0500

    signUp made functional, user created from front-end

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,39 +1,85 @@
-import { Link } from "react-router-dom";
-const SignUp = () => {
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+// import OAuth from "../components/OAuth";
+
+export default function SignUp() {
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      console.log(data);
+      if (data.success === false) {
+        setLoading(false);
+        setError(data.message);
+        return;
+      }
+      setLoading(false);
+      setError(null);
+      navigate("/sign-in");
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
+    }
+  };
   return (
-    <div className="p- max-w-lg mx-auto">
-      <h1 className="my-7 text-3xl text-center font-semibold">Sign Up</h1>
-      <form className="flex flex-col gap-4 ">
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
         <input
           type="text"
+          placeholder="username"
+          className="border p-3 rounded-lg"
           id="username"
-          placeholder="username.."
-          className="p-3 border rounded-lg "
+          onChange={handleChange}
         />
         <input
-          className="p-3 border rounded-lg "
-          type="text"
-          id="username"
-          placeholder="username.."
+          type="email"
+          placeholder="email"
+          className="border p-3 rounded-lg"
+          id="email"
+          onChange={handleChange}
         />
         <input
-          className="p-3 border rounded-lg "
-          type="text"
-          id="username"
-          placeholder="username.."
+          type="password"
+          placeholder="password"
+          className="border p-3 rounded-lg"
+          id="password"
+          onChange={handleChange}
         />
-        <button className="uppercase bg-slate-700 text-white p-3 rounded-lg hover:opacity-95 disabled:opacity-80">
-          Sign up
+
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+        >
+          {loading ? "Loading..." : "Sign Up"}
         </button>
+        {/* <OAuth /> */}
       </form>
-      <div className="flex gap-2 my-2">
-        <p>Have account?</p>
-        <Link to={"/sign-up"}>
-          <span className="text-blue-700">sign in</span>
+      <div className="flex gap-2 mt-5">
+        <p>Have an account?</p>
+        <Link to={"/sign-in"}>
+          <span className="text-blue-700">Sign in</span>
         </Link>
       </div>
+      {error && <p className="text-red-500 mt-5">{error}</p>}
     </div>
   );
-};
-
-export default SignUp;
+}
