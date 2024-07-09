ca2e16410495aa00584248053bc9563726ad4b86
Syed Asifullah <syedasifullah00@gmail.com>
2024-07-09 11:07:26 +0500

    create signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../modals/users.modal.js";
 import bcryptjs from "bcrypt";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 export const signUp = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashedPass = bcryptjs.hashSync(password, 10);
@@ -11,6 +12,26 @@ export const signUp = async (req, res, next) => {
     res.status(201).json("user created successfully!!");
   } catch (error) {
     // res.status(500).json(error.message);
+    next(error);
+  }
+};
+
+export const signIn = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    const validUser = await User.findOne({ email });
+    const validPass = bcryptjs.hashSync(password, validUser.password);
+    if (!validUser) return next(errorHandler(404, "user not found"));
+    if (!validPass) return next(errorHandler(401, "wrong creds!!"));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: pass, ...rest } = validUser._doc;
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
+
+    console.log(validUser._doc);
+  } catch (error) {
     next(error);
   }
 };
